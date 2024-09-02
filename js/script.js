5e4b48a6c6e00483b4edfd50a34f93e0ffcec418
syayidabdan <syayidabdann@gmail.com>
2024-09-02 10:50:41 +0700

    gatau

--- js/script.js
+++ js/script.js
@@ -47,7 +47,7 @@ ScrollReveal().reveal('.home-content p,.about-content', { origin: 'right' });
 
 // typed js
 const typed = new Typed('.multiple-text',{
-    strings:['Web Developer.','Android Developer.','Graphic Designer.'],
+    strings:['Web Developer.','Android Developer.','UI/UX Designer.'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
